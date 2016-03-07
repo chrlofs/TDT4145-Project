038a7abc8b9fe6e748b0d9070213f00d4b4164cf
Christoffer Lofsberg <chris.lofsberg@gmail.com>
2016-03-07 11:50:22 +0100

    Add test String

--- view/src/View.java
+++ view/src/View.java
@@ -1,5 +1,20 @@
 
 public class View {
 
-    
+    public void mainMenu() {
+
+        String welcome = "Velkommen til denne treningsdagboken! \n";
+        welcome += "Vennligst velg din handling ved å taste riktig tallkode under, etterfulgt av enter: \n";
+        welcome += "1 - Legg inn ny treningsøkt \n";
+        welcome += "2 - Hent ut tidligere treningsøkter \n";
+        welcome += "\n For å avslutte, trykk hvilken som helst annen tast";
+
+        System.out.print(welcome);
+    }
+
+    public static void main(String[] args) {
+        View main = new View();
+        main.mainMenu();
+    }
+
 }
