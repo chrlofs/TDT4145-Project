1b60f8d428d871cfc78c2e4eb9bc428a8e7b92a1
Christoffer Lofsberg <chris.lofsberg@gmail.com>
2016-03-14 11:28:19 +0100

    started with gui

--- view/src/View.java
+++ view/src/View.java
@@ -5,9 +5,9 @@ public class View {
 
         String welcome = "Velkommen til denne treningsdagboken! \n";
         welcome += "Vennligst velg din handling ved å taste riktig tallkode under, etterfulgt av enter: \n";
-        welcome += "1 - Legg inn ny treningsøkt \n";
-        welcome += "2 - Hent ut tidligere treningsøkter \n";
-        welcome += "\n For å avslutte, trykk hvilken som helst annen tast";
+        welcome += "1 - Legg inn ny treningsøkt\n";
+        welcome += "2 - Hent ut tidligere treningsøkter\n";
+        welcome += "\nFor å avslutte, trykk hvilken som helst annen tast";
 
         System.out.print(welcome);
     }
