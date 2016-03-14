4de6fdf58c77f48b22da286ebf589aea3aa357ac
Håvard Anda Estensen <haavard.ae@gmail.com>
2016-03-14 22:18:16 +0100

    Add addTrainingSession()

--- TrainingDiary/src/Main.java
+++ TrainingDiary/src/Main.java
@@ -1,3 +1,4 @@
+import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
@@ -22,8 +23,31 @@ public class Main {
         return controller;
     }
 
-    private void addTraingingSession() {
+    private void addTrainingSession() {
+        // addTrainingSession(Date time, Integer duration, Integer shape, Integer rating, String note,
+        // String sportname, String type, String weather_type, double temperature, double humidity, Integer spectators)
+        // TODO: Add training session
 
+        while (true) {
+            // Loop for adding exercises
+            System.out.println("* betyr obligatorisk");
+            System.out.println("Legg inn antall sett*");
+            int sets = Integer.parseInt(scanner.nextLine());
+            System.out.println("Legg inn antall reps*");
+            int reps = Integer.parseInt(scanner.nextLine());
+            System.out.println("Legg inn vekt (kg)*");
+            int load = Integer.parseInt(scanner.nextLine());
+            System.out.println("Legg inn lengde");
+            int meters = Integer.parseInt(scanner.nextLine());
+            System.out.println("Legg inn varighet");
+            int seconds = Integer.parseInt(scanner.nextLine());
+            System.out.println("Legg inn navn");  // ??? Hvorfor ???
+            String name = scanner.nextLine();
+            System.out.println("Legg inn dato");
+            Date date = scanner.nextLine();  // Parse date somehow
+
+            controller.addExercisePerformed(sets, reps, load, meters, seconds, name, date);
+        }
     }
 
     private void collectSessions() {
@@ -64,6 +88,7 @@ public class Main {
                     // Do something
                     try {
                         Statement stmt = controller.conn.createStatement();
+                        // TODO: Remove query from main
                         ResultSet rs = stmt.executeQuery("SELECT training_session, Load, Reps, Sets FROM Resultat WHERE Name="+line2+" ORDER BY Load DESC limit "+10+";");
                         while(rs.next()) {
                             System.out.println("| "+rs.getDate("treningsøkt") + " | " + rs.getString("belastning") + " | " + rs.getInt("antall_rep") + " | " + rs.getInt("antall_sett") + " |");
@@ -77,6 +102,7 @@ public class Main {
                     // Do something
                     try {
                         Statement stmt = controller.conn.createStatement();
+                        // TODO: Remove query from main
                         ResultSet rs = stmt.executeQuery("SELECT training_session, Distance, Duration FROM Resultat WHERE Name="+line2+" ORDER BY Duration ASC limit "+10+";");
                         while(rs.next()) {
                             System.out.println("| "+rs.getDate("treningsøkt") + " | " + rs.getString("varighet") + " | " + rs.getInt("lengde") + " |" );
